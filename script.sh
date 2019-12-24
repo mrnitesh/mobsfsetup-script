d0d3074e8f8675c2dc7a5ec0d2caab4749b0145a
mrnitesh <nitesh.yours@gmail.com>
2019-12-24 17:06:51 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 # Date: 24/12/2019
 
 
-echo"Installing Python3"
+echo "Installing Python3"
 echo
 echo "Making your system Ready for mobsf installation."
 sudo apt-get install python3
@@ -11,9 +11,9 @@ echo
 echo "installing pip3"
 sudo apt install python3-pip
 echo
-echo"Installing JDK"
+echo "Installing JDK"
 echo
-echo"installing dependencies"
+echo "installing dependencies"
 sudo apt install python3-venv python3-pip python3-dev build-essential libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev wkhtmltopdf
 echo
 apt-get install python3-venv
