6475af9436247a3fa8a9147634230aa671a08b28
mrnitesh <nitesh.yours@gmail.com>
2019-12-24 17:10:31 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,9 +16,10 @@ echo
 echo "installing dependencies"
 sudo apt install python3-venv python3-pip python3-dev build-essential libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev wkhtmltopdf
 echo
-apt-get install python3-venv
+
 echo"installing mobsf"
 git clone https://github.com/MobSF/Mobile-Security-Framework-MobSF.git
+apt-get install python3-venv
 cd Mobile-Security-Framework-MobSF
 ./setup.sh 
 echo "setup completed"
